90675ab3ae78a67f93be330d84ef985107d9c2e4
mtmarctoni <marctonimas@protonmail.com>
2026-06-14 10:01:02 +0200

    remove accidental pnpm-workspace.yaml from commit

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -70,6 +70,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     title: t("defaultTitle"),
     description: t("defaultDescription"),
     path: "/",
+    locale,
   });
 }
 
@@ -96,21 +97,6 @@ export default async function LocaleLayout({ children, params }: Props) {
       <head>
         {cookiebotCbid && <CookiebotHead cbid={cookiebotCbid} />}
         {gtmId && <GTMHead gtmId={gtmId} />}
-
-        {/* Preconnect to external domains */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* Favicon */}
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-background font-sans antialiased`}
