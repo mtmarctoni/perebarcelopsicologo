95829118c0650b20e690936cbde6c5e2aca727be
mtmarctoni <marctonimas@protonmail.com>
2026-06-14 10:06:14 +0200

    refactor: replace custom GTM with @next/third-parties GoogleTagManager and clean up empty head
    - Remove GTMHead/GTMBody custom components in favor of GoogleTagManager
      from @next/third-parties/google (handles both script and noscript iframe)
    - Rename CookiebotHead -> CookiebotScript, change strategy to beforeInteractive
    - Remove empty <head> section (all font/favicon preloads handled by Next.js metadata API)
    - Add @next/third-parties dependency

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { notFound } from "next/navigation";
@@ -20,40 +21,13 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const CookiebotHead = ({ cbid }: { cbid: string }) => (
+const CookiebotScript = ({ cbid }: { cbid: string }) => (
   <Script
     id="Cookiebot"
     src="https://consent.cookiebot.com/uc.js"
     data-cbid={cbid}
     type="text/javascript"
-    strategy="afterInteractive"
-  />
-);
-
-const GTMHead = ({ gtmId }: { gtmId: string }) => (
-  <Script
-    id="gtm-script"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${gtmId}');
-      `,
-    }}
-  />
-);
-
-const GTMBody = ({ gtmId }: { gtmId: string }) => (
-  <noscript
-    dangerouslySetInnerHTML={{
-      __html: `
-        <iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe>
-      `,
-    }}
+    strategy="beforeInteractive"
   />
 );
 
@@ -94,28 +68,11 @@ export default async function LocaleLayout({ children, params }: Props) {
 
   return (
     <html lang={locale} suppressHydrationWarning className="scroll-smooth">
-      <head>
-        {/* Preconnect to external domains */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* Favicon */}
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-background font-sans antialiased`}
       >
-        {cookiebotCbid && <CookiebotHead cbid={cookiebotCbid} />}
-        {gtmId && <GTMHead gtmId={gtmId} />}
-        {gtmId && <GTMBody gtmId={gtmId} />}
+        {cookiebotCbid && <CookiebotScript cbid={cookiebotCbid} />}
+        {gtmId && <GoogleTagManager gtmId={gtmId} />}
         <Providers>
           <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
         </Providers>
