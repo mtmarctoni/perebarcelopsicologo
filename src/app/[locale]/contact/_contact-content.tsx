fd6fdbd6e52719d3176748383d2340ada1f07cce
mtmarctoni <marctonimas@protonmail.com>
2026-06-14 10:48:34 +0200

    refactor: convert wrapper components and Footer to Server Components
    - Remove 'use client' from page wrapper files (_home-content, _about-content, _contact-content, _privacy-content, _servicios-content)
    - Convert Footer to async Server Component using getTranslations from next-intl/server
    - Convert CalendlyBookingCard to async Server Component using getTranslations
    - Use getLocale() from next-intl/server to get current locale without prop drilling
    - Minimizes client JS bundle by rendering static content server-side

--- src/app/[locale]/contact/_contact-content.tsx
+++ src/app/[locale]/contact/_contact-content.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import ContactFormSection from "@/components/contact/ContactFormSection";
 import ContactHeroSection from "@/components/contact/ContactHeroSection";
 import ContactSocialSection from "@/components/contact/ContactSocialSection";
