70737bd9197954a617bff76b08b85b535c9ef02a
mtmarctoni <marctonimas@protonmail.com>
2026-06-19 18:38:27 +0200

    fix(knip): remove unused export EXPERIENCE_START_YEAR

--- src/utils/experience.ts
+++ src/utils/experience.ts
@@ -1,4 +1,4 @@
-export const EXPERIENCE_START_YEAR = 2018;
+const EXPERIENCE_START_YEAR = 2018;
 
 export function getYearsOfExperience(): number {
   return new Date().getFullYear() - EXPERIENCE_START_YEAR;
