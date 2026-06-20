2baaa93b3ce24f13167a19699f0dcc07adacada5
mtmarctoni <marctonimas@protonmail.com>
2026-06-20 13:47:21 +0200

    fix(react-doctor): remove unused baseUrl export

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,4 @@
 const domain: string = "perebarcelopsicologo.com";
 export const phone: string = "+34 636 67 47 59";
 export const email: string = "pere@perebarcelopsicologo.com";
-export const baseUrl: string = "";
 export const wordpressUrl: string = `https://blog.${domain}`;
